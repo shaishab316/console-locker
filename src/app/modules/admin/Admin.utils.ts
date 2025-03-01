6ba49dcb0045719399e70393a31fdd4d59ac3cd0
shaishabcoding <shaishabchandrashil@gmail.com>
2025-03-01 18:19:49 +0600

    fixed

--- src/app/modules/admin/Admin.utils.ts
+++ src/app/modules/admin/Admin.utils.ts
@@ -1,13 +1,13 @@
-import rateLimit from "express-rate-limit";
-import ApiError from "../../../errors/ApiError";
-import { StatusCodes } from "http-status-codes";
+import rateLimit from 'express-rate-limit';
+import ApiError from '../../../errors/ApiError';
+import { StatusCodes } from 'http-status-codes';
 
 export const generateOtp = () =>
   Math.floor(1_00_000 + Math.random() * 9_00_000);
 
 export const limiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 10 minutes window
-  limit: 5, // Allow 5 requests per 10 minutes
+  windowMs: 60 * 1000, // 1-minute window
+  limit: 100, // Allow 100 requests per minute
   handler: () => {
     throw new ApiError(
       StatusCodes.TOO_MANY_REQUESTS,
